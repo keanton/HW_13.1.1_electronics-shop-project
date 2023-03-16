c117db74e9c50a89e4135f1fb78654647a470191
Toha <you@example.com>
2023-03-16 23:21:58 +0300

    add test

--- src/item.py
+++ src/item.py
@@ -2,7 +2,7 @@ class Item:
     """
     Класс для представления товара в магазине.
     """
-    pay_rate = 1.0
+    pay_rate = 0.85
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
@@ -13,7 +13,11 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +25,28 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price * Item.pay_rate
+        return self.price
+
+item1 = Item("Смартфон", 1000, 20)
+item2 = Item("Ноутбук", 20000, 5)
+
+print(item1.calculate_total_price())
+print(item2.calculate_total_price())
+# 200000 # общая стоимость смартфонов
+# 100000 # общая стоимость ноутбуков
+
+Item.pay_rate = 0.8  # устанавливаем новый уровень цен
+item1.apply_discount()
+print(item1.price)
+print(item2.price)
+# 8000.0 # к цене смартфона применена скидка
+# 20000 # к цене ноутбука скидка не была применена
+
+print(Item.all)
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+
+from src.item import Item
+
+@pytest.fixture
+def item1():
+    return Item("test", 10, 10)
+
+
+def test_calculate_total_price(item1):
+    assert item1.calculate_total_price() == 100
+
+def test_apply_discount(item1):
+    assert item1.apply_discount() == 8
